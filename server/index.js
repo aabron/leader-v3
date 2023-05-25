d9eb20d409e089eff525f630926ca8e5c0e10107
Aaron Perel <a.a.ron.perel9@gmail.com>
2023-05-25 19:29:19 -0400

    created index.js in server

---
+++ server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "corse";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+//Config
+const _fileName = fileURLToPath(import.meta.url);
+const _dirName = path.dirname(_fileName);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+app.use("/assets", express.static(path.join(_dirName, 'public/assets')));
+
